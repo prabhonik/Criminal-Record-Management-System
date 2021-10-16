9409fed2ebbd7879743c17bddf31647d1179c889
Nikhil Prabhu K <74901434+prabhonik@users.noreply.github.com>
2021-10-16 17:18:43 +0530

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,7 @@
  <?php
- $host="localhost";
-  $user="root";
-  $pass="";
-  $db="criminalrecord";
+ $host="ec2-44-196-244-150.compute-1.amazonaws.com";
+  $user="xyyffeifijayku";
+  $pass="6ffb44397572bd2cce65004728019a8e8e3af6418e2fc304a11f8581f4bc2364";
+  $db="das9qeidle2uji";
   $conn=mysqli_connect($host,$user,$pass,$db) or die("we couldn't connect");
   ?>
